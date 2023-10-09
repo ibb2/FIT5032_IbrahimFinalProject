9664f1aac6d365606bc12ed1ea02d07ba116069c
ibb2 <70052141+ibb2@users.noreply.github.com>
2023-10-09 23:33:35 +1100

    Reinit with .net framework

---
+++ FIT5032_IbrahimFinalProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_IbrahimFinalProject.Startup))]
+namespace FIT5032_IbrahimFinalProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
