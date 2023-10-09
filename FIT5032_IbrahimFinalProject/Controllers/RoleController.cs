e485a3b3095b967b1a4d4e1204b06363c53d0c07
ibb2 <70052141+ibb2@users.noreply.github.com>
2023-10-10 02:53:36 +1100

    3 roles created and present. User, Staff, Admin

---
+++ FIT5032_IbrahimFinalProject/Controllers/RoleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FIT5032_IbrahimFinalProject.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager; 
+        public RoleController(RoleManager<IdentityRole> roleManager) { 
+            this._roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+        public IActionResult Create() { 
+            return View(new IdentityRole()); 
+        }
+        [HttpPost] public async Task<IActionResult> Create(IdentityRole role) { 
+            await _roleManager.CreateAsync(role); 
+            return RedirectToAction("Index"); 
+        }
+    }
+}
