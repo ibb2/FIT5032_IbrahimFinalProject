4012a635463b19bfb203da16b1869649e8cd5413
ibb2 <70052141+ibb2@users.noreply.github.com>
2023-10-10 01:46:57 +1100

    Back to .net 6

---
+++ FIT5032_IbrahimFinalProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIT5032_IbrahimFinalProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ FIT5032_IbrahimFinalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FIT5032_IbrahimFinalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
